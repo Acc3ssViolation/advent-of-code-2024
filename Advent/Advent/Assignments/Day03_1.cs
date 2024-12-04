ce87cb8eea679ff95173a7920301144add149c6b
Acc3ss Violation <wrdbrookhuis@gmail.com>
2024-12-04 21:01:37 +0100

    Day 3

---
+++ Advent/Advent/Assignments/Day03_1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Advent.Assignments
+{
+    internal class Day03_1 : IAssignment
+    {
+        private class Parser
+        {
+            private enum State
+            {
+                IDLE,
+                LETTER_M,
+                LETTER_U,
+                LETTER_L,
+                PAR_OPEN,
+                DIGIT_A,
+                COMMA,
+                DIGIT_B,
+                PAR_CLOSE,
+            }
+
+            private State _state;
+            private int _argumentA;
+            private int _argumentB;
+
+            public bool PushChar(char c)
+            {
+                switch (_state)
+                {
+                    case State.IDLE:
+                        if (c == 'm')
+                            _state++;
+                        else
+                            _state = State.IDLE;
+                        break;
+                    case State.LETTER_M:
+                        if (c == 'u')
+                            _state++;
+                        else
+                            _state = State.IDLE;
+                        break;
+                    case State.LETTER_U:
+                        if (c == 'l')
+                            _state++;
+                        else
+                            _state = State.IDLE;
+                        break;
+                    case State.LETTER_L:
+                        if (c == '(')
+                            _state++;
+                        else
+                            _state = State.IDLE;
+                        break;
+                    case State.PAR_OPEN:
+                        if (char.IsAsciiDigit(c))
+                        {
+                            _argumentA = c - '0';
+                            _state++;
+                        }
+                        else
+                            _state = State.IDLE;
+                        break;
+                    case State.DIGIT_A:
+                        if (char.IsAsciiDigit(c))
+                        {
+                            _argumentA *= 10;
+                            _argumentA += c - '0';
+                        }
+                        else if (c == ',')
+                            _state++;
+                        else
+                            _state = State.IDLE;
+                        break;
+
+                    case State.COMMA:
+                        if (char.IsAsciiDigit(c))
+                        {
+                            _argumentB = c - '0';
+                            _state++;
+                        }
+                        else
+                            _state = State.IDLE;
+                        break;
+
+                    case State.DIGIT_B:
+                        if (char.IsAsciiDigit(c))
+                        {
+                            _argumentB *= 10;
+                            _argumentB += c - '0';
+                        }
+                        else if (c == ')')
+                        {
+                            _state++;
+                            return true;
+                        }
+                        else
+                            _state = State.IDLE;
+                        break;
+                }
+
+                return false;
+            }
+
+            public int Evaluate()
+            {
+                return _argumentA * _argumentB;
+            }
+
+            public void Clear()
+            {
+                _state = State.IDLE;
+            }
+        }
+
+        public string Run(IReadOnlyList<string> input)
+        {
+            var parser = new Parser();
+            var sum = 0;
+            foreach (var line in input)
+            {
+                foreach (var c in line)
+                {
+                    if (parser.PushChar(c))
+                    {
+                        sum += parser.Evaluate();
+                        parser.Clear();
+                    }
+                }
+            }
+            return sum.ToString();
+        }
+    }
+}
---
+++ Advent/Advent/Assignments/Day03_2.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Advent.Assignments
+{
+    internal class Day03_2 : IAssignment
+    {
+        private class FixedStringMatcher
+        {
+            private readonly string _needle;
+            private int _index;
+
+            public FixedStringMatcher(string needle)
+            {
+                _needle = needle ?? throw new ArgumentNullException(nameof(needle));
+            }
+
+            public bool PushChar(char c)
+            {
+                if (_needle[_index] == c)
+                {
+                    _index++;
+                    if (_index == _needle.Length)
+                    {
+                        _index = 0;
+                        return true;
+                    }
+                }
+                else
+                {
+                    _index = 0;
+                }
+                return false;
+            }
+        }
+
+        private class NumberMatcher
+        {
+            private bool _hasAny;
+            private int _number;
+
+            public bool PushChar(char c, out int result)
+            {
+                if (char.IsAsciiDigit(c))
+                {
+                    _number *= 10;
+                    _number += c - '0';
+                    result = _number;
+                    return false;
+                }
+                else if (_hasAny)
+                {
+                    result = _number;
+                    _hasAny = false;
+                    _number = 0;
+                    return true;
+                }
+                else
+                {
+                    result = _number;
+                    _number = 0;
+                    return true;
+                }
+            }
+        }
+
+        private class MulParser
+        {
+            private enum State
+            {
+                IDLE,
+                DIGIT_A,
+                DIGIT_B,
+            }
+
+            private FixedStringMatcher _prefixMatcher = new FixedStringMatcher("mul(");
+            private NumberMatcher _numberMatcher = new NumberMatcher();
+
+            private State _state;
+            private int _argumentA;
+            private int _argumentB;
+
+            public bool PushChar(char c)
+            {
+                switch (_state)
+                {
+                    case State.IDLE:
+                        if (_prefixMatcher.PushChar(c))
+                        {
+                            _argumentA = 0;
+                            _state++;
+                        }
+                        break;
+
+                    case State.DIGIT_A:
+                        if (_numberMatcher.PushChar(c, out var num))
+                        {
+                            if (c == ',')
+                            {
+                                _argumentA = num;
+                                _state++;
+                            }
+                            else
+                            {
+                                _state = State.IDLE;
+                            }
+                        }
+                        break;
+
+                    case State.DIGIT_B:
+                        if (_numberMatcher.PushChar(c, out var num2))
+                        {
+                            if (c == ')')
+                            {
+                                _argumentB = num2;
+                                _state = State.IDLE;
+                                return true;
+                            }
+                            else
+                            {
+                                _state = State.IDLE;
+                            }
+                        }
+                        break;
+                }
+
+                return false;
+            }
+
+            public int Evaluate()
+            {
+                return _argumentA * _argumentB;
+            }
+
+            public void Clear()
+            {
+                _state = State.IDLE;
+            }
+        }
+
+        public string TestFile => "test-day03-2.txt";
+
+        public string Run(IReadOnlyList<string> input)
+        {
+            var doMatcher = new FixedStringMatcher("do()");
+            var doNotMatcher = new FixedStringMatcher("don't()");
+            var parser = new MulParser();
+            var enabled = true;
+            var sum = 0;
+            foreach (var line in input)
+            {
+                foreach (var c in line)
+                {
+                    if (doMatcher.PushChar(c))
+                    {
+                        enabled = true;
+                    }
+                    if (doNotMatcher.PushChar(c))
+                    {
+                        enabled = false;
+                    }
+                    if (parser.PushChar(c))
+                    {
+                        if (enabled)
+                            sum += parser.Evaluate();
+
+                        parser.Clear();
+                    }
+                }
+            }
+            return sum.ToString();
+        }
+    }
+}
