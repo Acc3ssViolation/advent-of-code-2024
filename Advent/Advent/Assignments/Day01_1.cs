bbc3effb112e86b3d07736ac50671a5a490754e3
Acc3ss Violation <wrdbrookhuis@gmail.com>
2024-12-04 19:35:22 +0100

    Day 1

--- Advent/Advent/Assignments/Day01_1.cs
+++ Advent/Advent/Assignments/Day01_1.cs
@@ -1,24 +1,33 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Advent.Assignments
 {
     internal class Day01_1 : IAssignment
     {
-        private static readonly char[] Digits = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-
         public string Run(IReadOnlyList<string> input)
         {
-            var sum = 0;
+            var left = new List<int>(input.Count);
+            var right = new List<int>(input.Count);
 
             foreach (var item in input)
             {
-                var d0 = item.IndexOfAny(Digits);
-                var d1 = item.LastIndexOfAny(Digits);
+                var numbers = item.ExtractInts();
+                left.Add(numbers[0]);
+                right.Add(numbers[1]);
+            }
 
-                var v0 = item[d0] - '0';
-                var v1 = item[d1] - '0';
+            left.Sort();
+            right.Sort();
 
-                sum += 10 * v0 + v1;
+            Debug.Assert(left.Count == right.Count);
+
+            var sum = 0;
+            for (var i = 0; i < left.Count; i++)
+            {
+                var delta = Math.Abs(left[i] - right[i]);
+                sum += delta;
             }
 
             return sum.ToString();
--- Advent/Advent/Assignments/Day01_2.cs
+++ Advent/Advent/Assignments/Day01_2.cs
@@ -1,67 +1,72 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Advent.Assignments
 {
     internal class Day01_2 : IAssignment
     {
-        private static readonly string[] DigitStrings = new string[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-        private static readonly char[] Digits = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-
         public string Run(IReadOnlyList<string> input)
         {
-            var sum = 0;
+            var left = new List<int>(input.Count);
+            var right = new List<int>(input.Count);
 
             foreach (var item in input)
             {
-                var v0 = FindDigit(item);
-                var v1 = FindLastDigit(item);
+                var numbers = item.ExtractInts();
+                left.Add(numbers[0]);
+                right.Add(numbers[1]);
+            }
 
-                sum += 10 * v0 + v1;
+            left.Sort();
+            right.Sort();
+
+            Debug.Assert(left.Count == right.Count);
+
+            //for (var p = 0; p < left.Count; p++)
+            //{
+            //    Logger.DebugLine($"[{p:D3}] {left[p]}    {right[p]}");
+            //}
+
+            var sum = 0;
+            var i = 0;
+            var k = 0;
+
+            // Both lists are sorted, which we can use to move through both lists only once to find our answer
+            while (i < left.Count)
+            {
+                var number = left[i];
+                var count = 0;
+
+                // Skip over any numbers in the right list that are lower than the current number.
+                // We won't ever be seeing this number in the left list anymore due to the sorting, so we can skip them.
+                while (k < right.Count && right[k] < number)
+                {
+                    k++;
+                }
+
+                // Find how many occurences there are of the current number in the right list.
+                // Once we hit another number we know there are no more instances in the right list.
+                while (k < right.Count && right[k] == number)
+                {
+                    k++;
+                    count++;
+                }
+
+                // Count how many times the number happens in the left list, as we need to multiply that as well.
+                // This also ensures we move through the left list normally.
+                var iterations = 0;
+                while (i < left.Count && left[i] == number)
+                {
+                    i++;
+                    iterations++;
+                }
+
+                //Logger.DebugLine($"{number} (x{iterations}) appears {count} times, score {number * count} i={i} k={k}");
+                sum += number * count * iterations;
             }
 
             return sum.ToString();
-        }
-
-        private static int FindLastDigit(string input)
-        {
-            var highestIndex = -1;
-            var digit = 0;
-            for (var i = 0; i < DigitStrings.Length; i++)
-            {
-                var digitIndex0 = input.LastIndexOf(Digits[i]);
-                var digitIndex1 = input.LastIndexOf(DigitStrings[i]);
-                var index = digitIndex0 > digitIndex1 ? digitIndex0 : digitIndex1;
-                if (index > highestIndex)
-                {
-                    highestIndex = index;
-                    digit = i;
-                }
-            }
-
-            return digit;
-        }
-
-        private static int FindDigit(string input)
-        {
-            var lowestIndex = int.MaxValue;
-            var digit = 0;
-            for (var i = 0; i < DigitStrings.Length; i++)
-            {
-                var digitIndex0 = input.IndexOf(Digits[i]);
-                if (digitIndex0 < 0)
-                    digitIndex0 = int.MaxValue;
-                var digitIndex1 = input.IndexOf(DigitStrings[i]);
-                if (digitIndex1 < 0)
-                    digitIndex1 = int.MaxValue;
-                var index = digitIndex0 < digitIndex1 ? digitIndex0 : digitIndex1;
-                if (index < lowestIndex)
-                {
-                    lowestIndex = index;
-                    digit = i;
-                }
-            }
-
-            return digit;
         }
     }
 }
