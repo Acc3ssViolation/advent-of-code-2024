918a100c65290e68bd33715c93db93c4ebf35a4a
Acc3ss Violation <wrdbrookhuis@gmail.com>
2024-12-04 21:30:23 +0100

    Day 4

--- Advent/Advent/Assignments/Day04_1.cs
+++ Advent/Advent/Assignments/Day04_1.cs
@@ -1,16 +1,111 @@
-﻿using System;
+﻿using Advent.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Advent.Assignments
 {
     internal class Day04_1 : IAssignment
     {
+        private class ShapeMatcher4x4
+        {
+            private char[] _chars = new char[4 * 4];
+
+            public ShapeMatcher4x4(string shape)
+            {
+                var chars = shape.Split(Environment.NewLine).SelectMany(s => s).ToArray();
+                chars.CopyTo(_chars, 0);
+            }
+
+            public bool Match(Point point, CharGrid grid)
+            {
+                for (var i = 0; i < _chars.Length; i++)
+                {
+                    if (_chars[i] == '.')
+                        continue;
+
+                    var x = i % 4 + point.x;
+                    var y = i / 4 + point.y;
+                    if (x >= grid.Width)
+                        return false;
+                    if (y >= grid.Height)
+                        return false;
+
+                    var index = x + y * grid.Height;
+                    var gridChar = grid.Chars[index];
+                    if (gridChar != _chars[i])
+                        return false;
+                }
+                return true;
+            }
+        }
+
         public string Run(IReadOnlyList<string> input)
         {
-            throw new NotImplementedException();
+            ShapeMatcher4x4[] matchers = [
+                new ShapeMatcher4x4("""
+                                    XMAS
+                                    ....
+                                    ....
+                                    ....
+                                    """),
+                new ShapeMatcher4x4("""
+                                    X...
+                                    .M..
+                                    ..A.
+                                    ...S
+                                    """),
+                new ShapeMatcher4x4("""
+                                    X...
+                                    M...
+                                    A...
+                                    S...
+                                    """),
+                new ShapeMatcher4x4("""
+                                    ...X
+                                    ..M.
+                                    .A..
+                                    S...
+                                    """),
+                new ShapeMatcher4x4("""
+                                    SAMX
+                                    ....
+                                    ....
+                                    ....
+                                    """),
+                new ShapeMatcher4x4("""
+                                    S...
+                                    .A..
+                                    ..M.
+                                    ...X
+                                    """),
+                new ShapeMatcher4x4("""
+                                    S...
+                                    A...
+                                    M...
+                                    X...
+                                    """),
+                new ShapeMatcher4x4("""
+                                    ...S
+                                    ..A.
+                                    .M..
+                                    X...
+                                    """),
+            ];
+            var grid = new CharGrid(input);
+            var matchCount = 0;
+            for (var y = 0; y < grid.Height; y++)
+            {
+                for (var x = 0; x < grid.Width; x++)
+                {
+                    foreach (var matcher in matchers)
+                    {
+                        if (matcher.Match(new Point(x, y), grid))
+                            matchCount++;
+                    }
+                }
+            }
+            return matchCount.ToString();
         }
     }
 }
---
+++ Advent/Advent/Assignments/Day04_2.cs
@@ -0,0 +1,83 @@
+﻿using Advent.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent.Assignments
+{
+    internal class Day04_2 : IAssignment
+    {
+        private class ShapeMatcher3x3
+        {
+            private char[] _chars = new char[3 * 3];
+
+            public ShapeMatcher3x3(string shape)
+            {
+                var chars = shape.Split(Environment.NewLine).SelectMany(s => s).ToArray();
+                chars.CopyTo(_chars, 0);
+            }
+
+            public bool Match(Point point, CharGrid grid)
+            {
+                for (var i = 0; i < _chars.Length; i++)
+                {
+                    if (_chars[i] == '.')
+                        continue;
+
+                    var x = i % 3 + point.x;
+                    var y = i / 3 + point.y;
+                    if (x >= grid.Width)
+                        return false;
+                    if (y >= grid.Height)
+                        return false;
+
+                    var index = x + y * grid.Height;
+                    var gridChar = grid.Chars[index];
+                    if (gridChar != _chars[i])
+                        return false;
+                }
+                return true;
+            }
+        }
+
+        public string Run(IReadOnlyList<string> input)
+        {
+            ShapeMatcher3x3[] matchers = [
+                new ShapeMatcher3x3("""
+                                    M.M
+                                    .A.
+                                    S.S
+                                    """),
+                new ShapeMatcher3x3("""
+                                    M.S
+                                    .A.
+                                    M.S
+                                    """),
+                new ShapeMatcher3x3("""
+                                    S.S
+                                    .A.
+                                    M.M
+                                    """),
+                new ShapeMatcher3x3("""
+                                    S.M
+                                    .A.
+                                    S.M
+                                    """),
+            ];
+            var grid = new CharGrid(input);
+            var matchCount = 0;
+            for (var y = 0; y < grid.Height; y++)
+            {
+                for (var x = 0; x < grid.Width; x++)
+                {
+                    foreach (var matcher in matchers)
+                    {
+                        if (matcher.Match(new Point(x, y), grid))
+                            matchCount++;
+                    }
+                }
+            }
+            return matchCount.ToString();
+        }
+    }
+}
