eca982ff053b1b844d6e7285fde51e384f91e84d
Acc3ss Violation <wrdbrookhuis@gmail.com>
2024-12-13 14:32:40 +0100

    Day 13.2

---
+++ Advent/Advent/Assignments/Day13_2.cs
@@ -0,0 +1,54 @@
+﻿using Advent.Shared;
+using System;
+using System.Collections.Generic;
+
+namespace Advent.Assignments
+{
+    internal class Day13_2 : IAssignment
+    {
+        private readonly record struct ClawMachine(RealPoint A, RealPoint B, RealPoint Prize);
+
+        public string Run(IReadOnlyList<string> input)
+        {
+            var globalTokens = 0L;
+            for (var i = 0; (i + 2) < input.Count; i += 4)
+            {
+                var a = input[i + 0].ExtractInts();
+                var b = input[i + 1].ExtractInts();
+                var p = input[i + 2].ExtractInts();
+                var machine = new ClawMachine(new RealPoint(a[0], a[1]), new RealPoint(b[0], b[1]), new RealPoint(p[0] + 10000000000000, p[1] + 10000000000000));
+                var tokens = FindMinTokens(ref machine);
+                globalTokens += tokens;
+            }
+            return globalTokens.ToString();
+        }
+
+        private static long FindMinTokens(ref ClawMachine machine)
+        {
+            var a = machine.A;
+            var b = machine.B;
+            var p = machine.Prize;
+
+            // Just worked this out on paper, it's just a linear system with two variables N and M:
+            // N * Ax + M * Bx = Px
+            // N * Ay + M * By = Py
+            // If N and M are integers it is solvable, otherwise it isn't
+
+            var m = (p.y - ((a.y * p.x) / a.x)) / (b.y - ((a.y * b.x) / a.x));
+            var n = (p.x / a.x) - (b.x / a.x) * m;
+
+            var mInt = (long)Math.Round(m);
+            var nInt = (long)Math.Round(n);
+            if (Math.Abs(m - mInt) < 0.001 && Math.Abs(n - nInt) < 0.001)
+            {
+                //Logger.DebugLine($"{machine} | N = {nInt} | M = {mInt} | SOLVABLE");
+                return nInt * 3 + mInt;
+            }
+            else
+            {
+                //Logger.DebugLine($"{machine} | N = {n} | M = {m} | IMPOSSIBLE");
+                return 0;
+            }
+        }
+    }
+}
